852adf8832a56475eec9d2b0366c13149e61bfb9
themusaigen <>
2024-09-07 12:56:57 +0300

    Added examples for standalone modules, added Services folder in examples.project.json

---
+++ examples/server/Standalone.server.lua
@@ -0,0 +1,11 @@
+-- Standalone version of Function, outside of Talkie`s class.
+local Function = require(game.ReplicatedStorage.Talkie.Function)
+
+-- Useful then you use only one entity in service/controller/module/etc
+local someCoolFun = Function.new(game.ReplicatedStorage, "SomeFun")
+
+-- Same working with other modules
+local Event = require(game.ReplicatedStorage.Talkie.Event)
+
+-- Use standalone version of Event
+local someCoolEvent = Event.new(game.ReplicatedStorage, "SomeEvent")
